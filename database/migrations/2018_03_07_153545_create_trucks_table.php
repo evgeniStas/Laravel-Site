24a814e365559252c7c4af5c14b0afbb45a16dc8
evgeniStas <evgeni_stas@swiego.com>
2018-03-07 17:39:01 +0200

    Trucks migration

---
+++ database/migrations/2018_03_07_153545_create_trucks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTrucksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trucks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('length');
+            $table->string('capacity');
+            $table->double('price', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trucks');
+    }
+}
